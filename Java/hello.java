e1ecff9643b604ed4d74b0b15dd72f9d1d0c3798
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2024-04-25 15:36:38 +0200

    save

--- Java/hello.java
+++ Java/hello.java
@@ -1,5 +1,24 @@
-class MyProgram {
+class CarProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        int price = 8750;
+        byte doors = 5;
+        boolean isUsed = true;
+        boolean isAutomatic = false;
+        int km = 41000;
+        byte cubicCapacity = 3;
+        char co2 = 'B';
+        double fuelConsomption = 7.65;
+        short power = 492;
+
+        System.out.println("price: " + price);
+        System.out.println("doors: " + doors);
+        System.out.println("isUsed: " + isUsed);
+        System.out.println("isAutomatic: " + isAutomatic);
+        System.out.println("km: " + km);
+        System.out.println("cubicCapacity: " + cubicCapacity);
+        System.out.println("co2: " + co2);
+        System.out.println("fuelConsomption: " + fuelConsomption);
+        System.out.println("power: " + power);
     }
 }
