5550be079ce5c2bbcc3a5a7114eb691e7b65ae78
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2024-06-19 16:16:53 +0200

    save

--- Java/eclipse-workspace/tutorial/src/tutorial/Main.java
+++ Java/eclipse-workspace/tutorial/src/tutorial/Main.java
@@ -1,9 +1,13 @@
 package tutorial;
 
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Date now = new Date();
+		System.out.println(now);
+
 	}
-	
+
 }
